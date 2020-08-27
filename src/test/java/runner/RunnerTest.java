0454b5e33d33670ab0317286afbc6c4af3ef166d
OEM Configuration (temporary user) <joao.amaral@tqi.com.br>
2020-08-27 19:49:44 -0300

    adjust test

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -4,19 +4,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-import static steps.FichaImovelSteps.*;
-import static steps.HomeBuscaSteps.*;
-import static steps.HomeFiltrosSteps.*;
-import static steps.HomeFooterSteps.*;
-import static steps.HomeHeaderSteps.*;
-import static steps.ResultadosBuscaSteps.*;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
         plugin = {"usage:target/usage"
                 , "pretty"
-                , "json:target/reports/CucumberReport.json"},
+                , "json:target/reports/CucumberReport.json"
+                , "html:target/html/"
+                , "junit:target/cucumber-reports/Cucumber.xml"},
         glue = {"steps"},
         monochrome = true,
         tags = {"@REGRESSAO"}
