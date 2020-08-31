9d8b554919769ae2ada98f01d101913926847944
OEM Configuration (temporary user) <joao.amaral@tqi.com.br>
2020-08-31 10:50:14 -0300

    adjust pom

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/features"},
         plugin = {
-                "pretty"
-                , "json:target/reports/CucumberReport.json"},
+                "pretty",
+                "json:target/reports/CucumberReport.json"},
         glue = {"steps"},
         tags = {"@REGRESSAO"}
 )
