b61f9486720533281d73e97d8bd7bde726b5d9c8
Keflin <keflinduarte@gmail.com>
2021-02-01 10:15:14 -0300

    GROWTH-188 - Calculadora

---
+++ src/test/java/pages/Calculadora.java
@@ -0,0 +1,93 @@
+package pages;
+
+import support.Setup;
+
+public class Calculadora {
+
+    private Setup driver;
+
+    public Calculadora(Setup stepDriver) {
+        driver = stepDriver;
+    }
+
+    public void addRendaFamiliar(String value) {
+        driver.sendKeys(value, "moneyAmount");
+    }
+
+    public void addEntrada(String value) {
+        driver.sendKeys(value, "downPayment");
+    }
+
+    public void addFGTS(String value) {
+        driver.sendKeys(value, "fgts");
+    }
+
+    public void clicarCalcular() {
+        driver.forceClick("//button[@class='submit-button__primary submit-button']", "xpath");
+    }
+
+    public void clicarImoveisProntos() {
+        driver.forceClick("//div[@class='tab-switcher__tab ng-star-inserted']", "xpath");
+    }
+
+    public void verificarFormulario() {
+        driver.findElem("//div[@class='concierge-lead']", "xpath").isDisplayed();
+    }
+
+    public void inserirNome(String value) {
+        driver.sendKeys(value, "name");
+    }
+
+    public void inserirEstado(String value) throws InterruptedException {
+        driver.forceClick("state");
+//        WebElement select = driver.findElem("//ul[@class='ant-select-dropdown-menu ant-select-dropdown-menu-root ant-select-dropdown-menu-vertical']", "xpath");
+//
+//        List<WebElement> options = select.findElements(By.tagName("li"));
+//
+//       for (WebElement option : options) {
+//            if (value.equals(option.getText())) {
+//                option.click();
+//                driver.forceClick("state");
+//            }
+//        }
+
+//        driver.sendKeys(value, "state");
+        driver.forceClick("//*[@id=\"cdk-overlay-1\"]/div/div/ul/li[1]", "xpath");
+    }
+
+    public void inserirCidade(String value) {
+        driver.forceClick("city");
+        driver.forceClick("//*[@id=\"cdk-overlay-2\"]/div/div/ul/li[22]", "xpath");
+    }
+
+    public void inserirTelefone(String value) {
+        driver.sendKeys(value, "phone");
+    }
+
+    public void inserirEmail(String value) {
+        driver.sendKeys(value, "email");
+    }
+
+    public void selecionarContato(String value) {
+        switch (value) {
+            case "Whatsapp":
+                driver.forceClick("/html/body/lps-root/lps-calculator/lps-application-layout/nav-layout-default/div/div[2]/div/div[2]/div/lps-secondary-market-lead-form/lps-concierge-lead-form/div/form/nz-radio-group/label[1]", "xpath");
+                break;
+            case "E-mail":
+                driver.forceClick("/html/body/lps-root/lps-calculator/lps-application-layout/nav-layout-default/div/div[2]/div/div[2]/div/lps-secondary-market-lead-form/lps-concierge-lead-form/div/form/nz-radio-group/label[2]", "xpath");
+                break;
+        }
+    }
+
+    public void clicarReceberSugestao() {
+        driver.forceClick("//button[@class='submit-button__secondary submit-button']", "xpath");
+    }
+
+    public void verificarMensagem(String value) {
+        driver.findElem("//p[@class='product-search__dropdown__title']", "xpath").isDisplayed();
+    }
+
+    public void clicarProcurarImoveis() {
+        driver.forceClick("//*[@id=\"product-search\"]/div/form/div[3]/lps-submit-button/button", "xpath");
+    }
+}
---
+++ src/test/java/steps/CalculadoraSteps.java
@@ -0,0 +1,88 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+import pages.Calculadora;
+import support.BaseSteps;
+
+public class CalculadoraSteps extends BaseSteps {
+
+    private static Calculadora calculadora = new Calculadora(driver);
+
+    @Dado("insiro {string} como Renda Familiar Mensal")
+    public void insiroComoRendaFamiliarMensal(String value) {
+        calculadora.addRendaFamiliar(value);
+    }
+
+    @E("insiro {string} como Entrada")
+    public void insiroComoEntrada(String value) {
+        calculadora.addEntrada(value);
+    }
+
+    @E("insiro {string} como FGTS")
+    public void insiroComoFGTS(String value) {
+        calculadora.addFGTS(value);
+    }
+
+    @E("clico no botão Calcular")
+    public void clicoNoBotãoCalcular() {
+        calculadora.clicarCalcular();
+    }
+
+    @Quando("clico em Imóveis Prontos")
+    public void clicoEmImóveisProntos() {
+        calculadora.clicarImoveisProntos();
+    }
+
+    @Então("visualizo o formulário do lead")
+    public void visualizoOFormulárioDoLead() {
+        calculadora.verificarFormulario();
+    }
+
+    @E("insiro meu nome {string}")
+    public void insiroMeuNome(String value) {
+        calculadora.inserirNome(value);
+    }
+
+    @E("seleciono meu estado {string}")
+    public void selecionoMeuEstado(String value) throws InterruptedException {
+        calculadora.inserirEstado(value);
+    }
+
+    @E("seleciono minha cidade {string}")
+    public void selecionoMinhaCidade(String value) throws InterruptedException {
+        calculadora.inserirCidade(value);
+    }
+
+    @E("insiro meu Telefone {string}")
+    public void insiroMeuTelefone(String value) {
+        calculadora.inserirTelefone(value);
+    }
+
+    @E("insiro meu E-mail {string}")
+    public void insiroMeuEMail(String value) {
+        calculadora.inserirEmail(value);
+    }
+
+    @E("seleciono minha preferencia de contato {string}")
+    public void selecionoMinhaPreferenciaDeContato(String value) {
+        calculadora.selecionarContato(value);
+    }
+
+    @E("clico em Receber sugestões")
+    public void clicoEmReceberSugestões() {
+        calculadora.clicarReceberSugestao();
+    }
+
+    @Então("visualizo a mensagem {string}")
+    public void visualizoAMensagem(String value) {
+        calculadora.verificarMensagem(value);
+    }
+
+    @Quando("clico em Procurar imóveis prontos")
+    public void clicoEmProcurarImóveisProntos() {
+        calculadora.clicarProcurarImoveis();
+    }
+}
